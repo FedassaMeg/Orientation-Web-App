f9ce180d2bec335a06f49bf0c9a6a77391689f1f
Thomas <fedassa.tadewos00@gmail.com>
2019-10-15 15:29:03 -0700

    Added Link to Plex videos for HIPPA Quiz

--- frontend/src/components/components/AccordionItem.js
+++ frontend/src/components/components/AccordionItem.js
@@ -1,11 +1,17 @@
 /**@jsx jsx */
 import { css, jsx } from "@emotion/core";
 import PropTypes from "prop-types";
+import { MdExpandMore, MdExpandLess } from "react-icons/md";
 
 export default function AccordionItem(props) {
   return (
-    <div>
-      <div onClick={() => props.handleOnClick(props.label)}>{props.label}</div>
+    <div css={container}>
+      <div css={header}>
+        <div css={section}>{props.label}</div>
+        <div onClick={() => props.handleOnClick(props.label)} css={expandBtn}>
+          {props.isOpen ? <MdExpandLess /> : <MdExpandMore />}
+        </div>
+      </div>
       {props.isOpen && <div>{props.children}</div>}
     </div>
   );
@@ -17,3 +23,26 @@ AccordionItem.propTypes = {
   label: PropTypes.string.isRequired,
   handleOnClick: PropTypes.func.isRequired
 };
+
+const container = css`
+  width: 350px;
+  padding-top: 8px;
+  padding-left: 36px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const header = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const section = css`
+  font-size: 16px;
+  font-weight: 400;
+`;
+const expandBtn = css`
+  margin-right: 16px;
+  padding-bottom: 4px;
+`;
