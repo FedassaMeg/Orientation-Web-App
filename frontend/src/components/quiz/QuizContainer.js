f154df1f84c6e460257f4e6aaec02f91ea1b3163
Thomas <fedassa.tadewos00@gmail.com>
2019-11-05 10:23:33 -0800

    Upgraded router-dom and added useHistory hook

--- frontend/src/components/quiz/QuizContainer.js
+++ frontend/src/components/quiz/QuizContainer.js
@@ -13,6 +13,7 @@
 */
 
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import axios from "axios";
 
@@ -21,6 +22,8 @@ import { ROOT_URL } from "../utils/constants";
 import Quiz from "./Quiz";
 
 export default function QuizContainer(props) {
+  let history = useHistory();
+
   const [dataArr, setDataArr] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
   const [inputMap, setInputMap] = useState(new Map());
@@ -122,7 +125,7 @@ export default function QuizContainer(props) {
     console.log(score);
     let config = {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`
       }
     };
 
@@ -137,6 +140,7 @@ export default function QuizContainer(props) {
       )
       .then(res => {
         console.log(res.data);
+        history.push("/quizs");
       })
       .catch(err => {
         console.log(err);
@@ -161,103 +165,3 @@ export default function QuizContainer(props) {
     />
   );
 }
-
-// import React, { Component } from "react";
-// import axios from "axios";
-// import { parseInt } from "lodash";
-
-// import Quiz from "./Quiz";
-// import { ROOT_URL } from "../utils/constants";
-
-// let scr = 0;
-// export default class QuizPost extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       answers: new Map(),
-//       questions: [],
-//       ansArray: []
-//     };
-//   }
-//   getQuestionsByQuizId = async () => {
-//     try {
-//       const { data } = await axios.get(
-//         `${ROOT_URL}/quizs/${this.props.quiz.key}/questions`
-//       );
-//       return data;
-//     } catch (err) {
-//       console.log(err.message);
-//     }
-//   };
-
-//   async componentDidMount() {
-//     const quizData = await this.getQuestionsByQuizId();
-//     this.setState({ questions: quizData });
-//     this.state.questions.map((question, index) => {
-//       this.setState({ ansArray: [...this.state.ansArray, question.answer] });
-//       this.setState({ answers: this.state.answers.set(index + 1, false) });
-//     });
-
-//     console.log(this.state);
-//     console.log(this.props.quiz.key);
-//   }
-//   handleChange = event => {
-//     const key = parseInt(event.target.id, 10);
-//     const checked = event.target.checked;
-//     const added = this.state.answers.set(key, checked);
-//     if (checked) {
-//       this.setState({
-//         added
-//       });
-//     }
-//     console.log(key);
-//     console.log(this.state.answers);
-//   };
-
-// compareAnsToScr = (value, key) => {
-//   if (value === this.state.ansArray[key - 1]) {
-//     scr = scr + 1;
-//   }
-// };
-
-// handleSubmit = event => {
-//   event.preventDefault();
-//   const quizId = this.props.quiz.key;
-//   this.state.answers.forEach(this.compareAnsToScr);
-//   console.log(this.state.answers);
-//   console.log(scr);
-//   let config = {
-//     headers: {
-//       Authorization: `Bearer ${localStorage.getItem("token")}`
-//     }
-//   };
-
-//   axios
-//     .post(
-//       `${ROOT_URL}scores/`,
-//       {
-//         score: scr,
-//         related_quiz: quizId
-//       },
-//       config
-//     )
-//     .then(res => {
-//       console.log(res);
-//       console.log(res.data);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
-
-//   render() {
-//     return (
-//       <Quiz
-//         quiz={this.props.quiz}
-//         // questions={this.state.questions}
-//         handleChange={this.handleChange}
-//         handleSubmit={this.handleSubmit}
-//       />
-//     );
-//   }
-// }
