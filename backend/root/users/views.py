6e348d9e895df98a2ea010cbf0ea73076fc81e2b
Thomas <fedassa.tadewos00@gmail.com>
2019-11-05 10:24:26 -0800

    removed permissions class on user view

--- backend/root/users/views.py
+++ backend/root/users/views.py
@@ -10,4 +10,4 @@ from .serializers import UserSerializer, CreateUserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
-    permission_classes=(IsAdminUser,)
+    
