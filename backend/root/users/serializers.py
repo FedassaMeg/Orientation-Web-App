a081726d695cd30e6e0a17b16f4f141de4276c3a
FedassaMeg <fedassa.tadewos00@gmail.com>
2019-11-06 05:01:08 +0000

    removed comments

--- backend/root/users/serializers.py
+++ backend/root/users/serializers.py
@@ -26,23 +26,3 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-
-# class CreateUserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CustomUser
-#         fields = ['password',
-#                   'first_name', 'last_name', 'role']
-#         extra_kwargs = {'password': {'write_only': True}}
-
-#     def create(self, validated_data):
-#         user = CustomUser(
-#             first_name=validated_data['first_name'],
-#             last_name=validated_data['last_name'],
-#             username=validated_data['first_name'][0].lower(
-#             ) + validated_data['last_name'].lower(),
-#             role=validated_data['role'],
-
-#         )
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
