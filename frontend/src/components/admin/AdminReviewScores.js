4d33aa93eec5a8e7650af88678d848f5e0606b80
Fedassa Megerssa <fedassa.tadewos00@gmail.com>
2019-12-22 18:10:35 -0800

    Move Jsx from container to presentation component

--- frontend/src/components/admin/AdminReviewScores.js
+++ frontend/src/components/admin/AdminReviewScores.js
@@ -4,14 +4,9 @@ import React, { useState, useLayoutEffect, useEffect } from "react";
 import { useAsync } from "react-async";
 
 import * as apiClient from "./api-call-admin";
-import Button from "../components/Button";
-import Card from "../components/Card";
-import ReviewQuiz from "./ReviewQuiz";
-import Table from "./table/Table";
-import TableHead from "./table/TableHead";
-import TableBody from "./table/TableBody";
-import TableRow from "./table/TableRow";
-import TableCell from "./table/TableCell";
+
+//Local components
+import ReviewScores from "./ReviewScores";
 import MuiReviewScoresTable from "./MuiReviewScoresTable";
 
 const rowdata = (scoreArr, userArr) => {
@@ -103,77 +98,16 @@ export default function AdminReviewScores(props) {
     }
   }
 
-  const headCells = [
-    {
-      id: "name",
-      label: "Employee Name"
-    },
-    { id: "quiz", label: "Quiz" },
-    { id: "score", label: "Quiz Score" },
-    {
-      id: "submit",
-      label: "Completed Date"
-    },
-    { id: "review", label: "Admin Review" }
-  ];
-
   return (
-    <div>
-      {!isClicked ? (
-        <Card header="Employee Quiz Scores">
-          <Table>
-            <TableHead>
-              <TableRow>
-                {headCells.map(headCell => (
-                  <TableCell key={headCell.id}>{headCell.label}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {scoreArray.map((rowdata, index) => {
-                let date = new Date(rowdata.signed_date);
-                return (
-                  <TableRow key={index}>
-                    {userArray.map((user, index) => {
-                      const userFullName =
-                        user.last_name + ", " + user.first_name;
-                      return (
-                        rowdata.signed_by == user.id && (
-                          <TableCell key={index} align="left">
-                            {userFullName}
-                          </TableCell>
-                        )
-                      );
-                    })}
-
-                    <TableCell>{rowdata.related_quiz.title}</TableCell>
-                    <TableCell>
-                      {rowdata.score}/{rowdata.related_quiz.num_questions}
-                    </TableCell>
-                    <TableCell>{date.toDateString()}</TableCell>
-                    <TableCell>
-                      {rowdata.related_quiz.review_required ? (
-                        <Button
-                          id={rowdata.related_quiz.id}
-                          name={rowdata.id}
-                          onClick={handleOnClick}
-                        >
-                          Review Quiz
-                        </Button>
-                      ) : (
-                        "Not Required"
-                      )}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </Card>
-      ) : (
-        <ReviewQuiz quizId={quizId} scoreId={scoreId} back={back} />
-      )}
-    </div>
+    <ReviewScores
+      isClicked={isClicked}
+      scoreArray={scoreArray}
+      userArray={userArray}
+      quizId={quizId}
+      scoreId={scoreId}
+      handleOnClick={handleOnClick}
+      back={back}
+    />
   );
 }
 
--- frontend/src/components/admin/ReviewScores.js
+++ frontend/src/components/admin/ReviewScores.js
@@ -1,2 +1,101 @@
 /**@jsx jsx */
 import { css, jsx } from "@emotion/core";
+
+//Local components
+import Button from "../components/Button";
+import Card from "../components/Card";
+import ReviewQuiz from "./ReviewQuiz";
+import Table from "./table/Table";
+import TableHead from "./table/TableHead";
+import TableBody from "./table/TableBody";
+import TableRow from "./table/TableRow";
+import TableCell from "./table/TableCell";
+
+const headCells = [
+  {
+    id: "name",
+    label: "Employee Name"
+  },
+  { id: "quiz", label: "Quiz" },
+  { id: "score", label: "Quiz Score" },
+  {
+    id: "submit",
+    label: "Completed Date"
+  },
+  { id: "review", label: "Admin Review" }
+];
+
+export default function ReviewScores(props) {
+  const {
+    isClicked,
+    scoreArray,
+    userArray,
+    quizId,
+    scoreId,
+    handleOnClick,
+    back
+  } = props;
+
+  return (
+    <div css={container}>
+      {!isClicked ? (
+        <Card header="Employee Quiz Scores">
+          <Table>
+            <TableHead>
+              <TableRow>
+                {headCells.map(headCell => (
+                  <TableCell key={headCell.id}>{headCell.label}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {scoreArray.map((rowdata, index) => {
+                let date = new Date(rowdata.signed_date);
+                return (
+                  <TableRow key={index}>
+                    {userArray.map((user, index) => {
+                      const userFullName =
+                        user.last_name + ", " + user.first_name;
+                      return (
+                        rowdata.signed_by == user.id && (
+                          <TableCell key={index} align="left">
+                            {userFullName}
+                          </TableCell>
+                        )
+                      );
+                    })}
+
+                    <TableCell>{rowdata.related_quiz.title}</TableCell>
+                    <TableCell>
+                      {rowdata.score}/{rowdata.related_quiz.num_questions}
+                    </TableCell>
+                    <TableCell>{date.toDateString()}</TableCell>
+                    <TableCell>
+                      {rowdata.related_quiz.review_required ? (
+                        <Button
+                          id={rowdata.related_quiz.id}
+                          name={rowdata.id}
+                          onClick={handleOnClick}
+                        >
+                          Review Quiz
+                        </Button>
+                      ) : (
+                        "Not Required"
+                      )}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Card>
+      ) : (
+        <ReviewQuiz quizId={quizId} scoreId={scoreId} back={back} />
+      )}
+    </div>
+  );
+}
+
+const container = css`
+  padding: 16px;
+`;
